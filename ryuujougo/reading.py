d36700e568710b29262a93bac88cc489f5ed7708
HSM <ntsp.gm@gmail.com>
2011-05-20 16:38:42 +1000

    Some refactoring and more info for solution segment output.

--- ryuujougo/reading.py
+++ ryuujougo/reading.py
@@ -5,134 +5,41 @@ import os
 import re
 import tools
 import copy
-import time
+
 from sqlalchemy.sql import select, and_, or_
-from sqlalchemy import create_engine, Table, Column, Unicode, \
-                       String, Boolean, ForeignKey, MetaData
 
-READINGS_PATH = "../dbs/readings.sqlite"
-JMDICT_PATH = "../dbs/jmdict.sqlite"
-KANJIDIC_PATH = "../dbs/kanjidic.sqlite"
-
-#Hiragana range is U+3041 to U+309F
-HIRAGANA_START = ord(unicode('ぁ'))
-HIRAGANA_END = ord(unicode('ゟ'))
-HIRAGANA_RANGE = range(HIRAGANA_START, HIRAGANA_END + 1)
-
-#Katakana range is U+30A0 to U+30FF
-KATAKANA_START = ord(unicode('゠'))
-KATAKANA_END = ord(unicode('ヿ'))
-KATAKANA_RANGE = range(KATAKANA_START, KATAKANA_END + 1)
-
-r_meta = MetaData()
-r_engine = None
-
-reading_t = Table('reading', r_meta,
-            Column('character', Unicode, primary_key=True),
-            Column('reading', Unicode, primary_key=True),
-            Column('type', String, primary_key=True),
-            Column('affix', String, primary_key=True),
-            Column('has_okurigana', Boolean, nullable=False))
-
-def init():
-    global r_engine
-    global r_meta
-
-    if not os.path.exists(READINGS_PATH):
-        if not os.path.exists(KANJIDIC_PATH):
-            print "No kanjidic database found at %s" % KANJIDIC_PATH
-            print "Cannot continue without it."
-            return
-        r_engine = create_engine('sqlite:///' + READINGS_PATH)
-        r_meta.create_all(r_engine)
-        db_populate_readings()
-    else:
-        r_engine = create_engine('sqlite:///' + READINGS_PATH)
-
-def db_populate_readings():
-    kd_engine = create_engine('sqlite:///' + KANJIDIC_PATH)
-    kd_meta = MetaData()
-    kd_meta.bind = kd_engine
-    kd_meta.reflect()
-
-    kd_reading = kd_meta.tables['reading']
-    kd_nanori = kd_meta.tables['nanori']
-
-    reading_l = []
-    n_to_save = 500
-    n = 0
-    start = time.time()
-
-    s = select([kd_reading], or_(kd_reading.c['r_type'] == 'ja_on',
-                                 kd_reading.c['r_type'] == 'ja_kun'))
-    readings = kd_engine.execute(s)
-    for r in readings:
-        affix = "none"
-        reading = r.reading
-        if r.reading[-1] == "-":
-            affix = "prefix"
-            reading = reading[:-1]
-        elif r.reading[0] == "-":
-            affix = "suffix"
-            reading = reading[1:]
-
-        has_oku = False
-        if "." in r.reading:
-            has_oku = True
-
-        reading_l.append({'character':r.character_literal,
-                          'reading':reading,
-                          'type':r.r_type,
-                          'affix':affix,
-                          'has_okurigana':has_oku})
-
-    s = select([kd_nanori])
-    nanori = kd_engine.execute(s)
-
-    for n in nanori:
-        reading_l.append({'character':n.character_literal,
-                          'reading':n.nanori,
-                           'type':'nanori',
-                           'affix':'none',
-                           'has_okurigana':False})
-
-    r_engine.execute(reading_t.insert(), reading_l)
-    print 'Filling database with kanji/reading data took '\
-            '%s seconds' % (time.time() - start)
+import tools
+import db
 
 
-def get_words_with_reading(kanji, kd_reading, words):
-    print "Kanji: %s  --  Reading: %s" % (kanji, kd_reading)
-    for entry in words:
-        (word, w_reading, usage, meaning) = entry
+class Segment():
+    unit = ''
+    reading = ''
+    info = ''
+    
+    def __init__(self, unit, reading, info=''):
+        self.unit = unit
+        self.reading = reading
+        self.info = info
+        
+    
 
-        for char in word:
-            if ord(char) in HIRAGANA_RANGE or ord(char) in KATAKANA_RANGE:
-                pass
-            else:
-                try:
-                    readings = get_individual_readings(str(char))
-                except:
-                    print "DIED ON %s" % char
-                    #readings = get_individual_readings(str(char))
-
-        #if reading in w_reading:
-            #print word, w_reading, usage, meaning
-
-
-
-
+indent = ''
+meta = db.get_db_meta()
+meta.reflect()
+reading_t = meta.tables['reading']
+r_engine = meta.bind
 def get_individual_readings(word, reading, segments=[], found="", solutions=[]):
     if len(word) == 0:
         print "Nothing left to solve. Appending this one as a solution."
         solutions.append(segments)
     else:
-        print "Solving: %s  =  %s          <--> Current attempt = %s" % (word, reading, found)
+        print "\nSolving: %s  ==  %s          <--> Root: %s" % (word, reading, found)
 
     #for char in word:
     if len(word) > 0:
         char = word[0]
-        if ord(char) in HIRAGANA_RANGE or ord(char) in KATAKANA_RANGE:
+        if tools.is_kana(char):
             #if ord(char) in KATAKANA_RANGE:
             #    char = tools.kata_to_hira(char)
             if len(reading) == 0:
@@ -144,22 +51,22 @@ def get_individual_readings(word, reading, segments=[], found="", solutions=[]):
                     tmp_word = word[1:]
                     tmp_reading = reading[1:]
                     tmp_segments = copy.copy(segments)
-                    tmp_segments.append([char, char])
-                    get_individual_readings(tmp_word, tmp_reading, tmp_segments, tmp_found, solutions)
+                    tmp_segments.append(Segment(char, char, 'kanji'))
+                    get_individual_readings(tmp_word, tmp_reading, tmp_segments,
+                                            tmp_found, solutions)
                 else:
                     print "%s is definitely not %s. Abort attempt." % (char, reading[0])
         else:
-
             s = select([reading_t], reading_t.c['character'] == char)
             char_readings = r_engine.execute(s).fetchall()
 
-            print "Character found: = " + char
+            print "0th character is a kanji: = " + char
 
             if char_readings == None:    #TODO: Handle non-kanji and non-kana characters
                 print "Need to abort gracefully!"
                 return None
             for r in char_readings:
-                print "\t" + r.reading + "\t" + r.affix + "\t" + str(r.has_okurigana)
+                print indent + "\t" + r.reading + "\t\t" + r.affix
 
             for cr in char_readings:
                 #print "     " + r.reading + \t + r.affix + \t + str(r.has_okurigana)
@@ -178,14 +85,14 @@ def get_individual_readings(word, reading, segments=[], found="", solutions=[]):
                         tmp_word = word[ol + 1:]
                         tmp_reading = reading[ol + rl:]
                         tmp_segments = copy.copy(segments)
-                        tmp_segments.append([char, r])
-                        tmp_segments.append([o, o])
+                        tmp_segments.append(Segment(char, r, 'kanji with okurigana'))
+                        tmp_segments.append(Segment(o, o, 'okurigana'))
                         get_individual_readings(tmp_word, tmp_reading, tmp_segments, tmp_found, solutions)
                 else:
                     r = cr.reading
                     rl = len(r)
                     #we deal with readings in hiragana, so convert from katakana if it exists
-                    if ord(r[0]) in KATAKANA_RANGE:
+                    if tools.is_kata(r[0]):
                         r = tools.kata_to_hira(r)
 
                     if reading.startswith(r):
@@ -194,34 +101,29 @@ def get_individual_readings(word, reading, segments=[], found="", solutions=[]):
                         tmp_word = word[1:]
                         tmp_reading = reading[rl:]
                         tmp_segments = copy.copy(segments)
-                        tmp_segments.append([char, r])
+                        tmp_segments.append(Segment(char, r, 'something'))
                         get_individual_readings(tmp_word, tmp_reading, tmp_segments, tmp_found, solutions)
             print "No further parsing possible for reading starting with %s and ending with %s.\n     Up one level." % (found, word)
     return solutions
 
 
 if __name__ == "__main__":
-    if False:
-        try:
-            os.remove(READINGS_PATH)
-        except:
-            pass
-    init()
+
     #db_populate_words_by_reading()
 
-#    soltions = get_individual_readings(u"小牛", u"こうし")
-#    soltions = get_individual_readings(u"お腹", u"おなか")
-#    soltions = get_individual_readings(u"バス停", u"バスてい")
-#    soltions = get_individual_readings(u"一つ", u"ひとつ")
-#    soltions = get_individual_readings(u"非常事態", u"ひじょうじたい")
-    soltions = get_individual_readings(u"建て替える", u"たてかえる")
-#    soltions = get_individual_readings(u"今日", u"きょう")
-#    soltions = get_individual_readings(u"小さい", u"ちいさい")
-#    soltions = get_individual_readings(u"鉄道公安官", u"てつどうこうあんかん")
-#    soltions = get_individual_readings(u"日帰り", u"ひがえり")
+#    solutions = get_individual_readings(u"小牛", u"こうし")
+#    solutions = get_individual_readings(u"お腹", u"おなか")
+#    solutions = get_individual_readings(u"バス停", u"バスてい")
+#    solutions = get_individual_readings(u"一つ", u"ひとつ")
+#    solutions = get_individual_readings(u"非常事態", u"ひじょうじたい")
+    solutions = get_individual_readings(u"建て替える", u"たてかえる")
+#    solutions = get_individual_readings(u"今日", u"きょう")
+#    solutions = get_individual_readings(u"小さい", u"ちいさい")
+#    solutions = get_individual_readings(u"鉄道公安官", u"てつどうこうあんかん")
+#    solutions = get_individual_readings(u"日帰り", u"ひがえり")
     print "\n\n"
-    for i, sol in enumerate(soltions):
+    for i,sol in enumerate(solutions):
         print "Solution #", i
-        for s in sol:
-            print s[0] + " -- " + s[1]
+        for seg in sol:
+             print "%s -- %s  -- %s" % (seg.unit, seg.reading, seg.info)
 #    db_populate_words_by_reading()
--- ryuujougo/tools.py
+++ ryuujougo/tools.py
@@ -10,6 +10,35 @@ KATAKANA_START = ord(unicode('゠'))
 KATAKANA_END = ord(unicode('ヿ'))
 KATAKANA_RANGE = range(KATAKANA_START, KATAKANA_END+1)
 
+KANA_RANGE = range(HIRAGANA_START, KATAKANA_END+1)
+
+def is_kata(s):
+    if len(s) > 0:
+        for char in s:
+            if ord(char) not in KATAKANA_RANGE:
+                return False
+        return True
+    else:
+        return False
+
+def is_hira(s):
+    if len(s) > 0:
+        for char in s:
+            if ord(char) not in HIRAGANA_RANGE:
+                return False
+        return True
+    else:
+        return False
+
+def is_kana(s):
+    if len(s) > 0:
+        for char in s:
+            if ord(char) not in KANA_RANGE:
+                return False
+        return True
+    else:
+        return False
+
 def kata_to_hira(kata): 
     hira = ""
     for char in unicode(kata):
@@ -28,3 +57,17 @@ def print_hira_kata():
     print ""
     for n in KATAKANA_RANGE:
         sys.stdout.write(unichr(n))
+
+if __name__ == '__main__':
+    #True
+    print is_kana(u'バスてい')
+    print is_hira(u'こんにちは')
+    print is_kata(u'コンピューター')
+    
+    #False
+    print is_kana(u'abc')
+    print is_kana(u'新しい')    
+    print is_hira(u'バスてい')
+    print is_kata(u'きょう')
+    print is_kata(u'きょうはスゴイ')
+    
