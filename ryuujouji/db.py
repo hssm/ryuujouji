ec36aa18860551216541399e6adb9b06dd82dc1b
HSM <ntsp.gm@gmail.com>
2011-05-21 19:04:48 +1000

    Added database tables.

--- ryuujouji/db.py
+++ ryuujouji/db.py
@@ -1,22 +1,41 @@
 import os
 import time
 from sqlalchemy import create_engine, Table, Column, Unicode, \
-                       String, Boolean, ForeignKey, MetaData
+                       String, Boolean, Integer, ForeignKey, MetaData
 from sqlalchemy.sql import select, and_, or_
 
 READINGS_PATH = "../dbs/readings.sqlite"
 JMDICT_PATH = "../dbs/jmdict.sqlite"
 KANJIDIC_PATH = "../dbs/kanjidic.sqlite"
 
+#readings
 r_meta = MetaData()
 r_engine = None
 
+
 reading_t = Table('reading', r_meta,
-            Column('character', Unicode, primary_key=True),
-            Column('reading', Unicode, primary_key=True),
-            Column('type', String, primary_key=True),
-            Column('affix', String, primary_key=True),
-            Column('has_okurigana', Boolean, nullable=False))
+                  Column('id', Integer, primary_key=True),
+                  Column('character', Unicode),
+                  Column('reading', Unicode),
+                  Column('type', String),
+                  Column('affix', String),
+                  Column('has_okurigana', Boolean, nullable=False))
+
+word_t = Table('word', r_meta,
+               Column('id', Integer, primary_key=True),
+               Column('keb', Unicode),
+               Column('reb', Unicode))
+
+solution_t = Table('solution', r_meta,
+                   Column('id', Integer, primary_key=True),
+                   Column('word_id', Integer, ForeignKey('word.id')))
+
+segment_t = Table('segment', r_meta,
+                   Column('id', Integer, primary_key=True),
+                   Column('solution_id', Integer, ForeignKey('solution.id')),
+                   Column('reading_id', Integer, ForeignKey('reading.id')),
+                   Column('index', Integer))
+
 
 def init():
     global r_engine
@@ -27,13 +46,20 @@ def init():
             print "No kanjidic database found at %s" % KANJIDIC_PATH
             print "Cannot continue without it."
             return
+        if not os.path.exists(JMDICT_PATH):
+            print "No jmdict database found at %s" % KANJIDIC_PATH
+            print "Cannot continue without it."
+            return
         r_engine = create_engine('sqlite:///' + READINGS_PATH)
-        r_meta.create_all(r_engine)
-        db_populate_readings()
+        r_meta.create_all(r_engine)       
+        db_populate_kanji_readings()
+        db_populate_words()
     else:
         r_engine = create_engine('sqlite:///' + READINGS_PATH)
+    
 
-def db_populate_readings():
+def db_populate_kanji_readings():
+    print "Filling database with kanji/reading data..."
     kd_engine = create_engine('sqlite:///' + KANJIDIC_PATH)
     kd_meta = MetaData()
     kd_meta.bind = kd_engine
@@ -43,8 +69,6 @@ def db_populate_readings():
     kd_nanori = kd_meta.tables['nanori']
 
     reading_l = []
-    n_to_save = 500
-    n = 0
     start = time.time()
 
     s = select([kd_reading], or_(kd_reading.c['r_type'] == 'ja_on',
@@ -81,23 +105,37 @@ def db_populate_readings():
                            'has_okurigana':False})
 
     r_engine.execute(reading_t.insert(), reading_l)
+
     print 'Filling database with kanji/reading data took '\
             '%s seconds' % (time.time() - start)
 
-def get_readings_meta():
+
+def db_populate_words():
+    print "Filling database with word/reading data..."
+    jd_engine = create_engine('sqlite:///' + JMDICT_PATH)
+    jd_meta = MetaData()
+    jd_meta.bind = jd_engine
+    jd_meta.reflect()
+
+    k_ele = jd_meta.tables['k_ele']
+    r_ele = jd_meta.tables['r_ele']
+
+    word_l = []
+    start = time.time()
+
+    s = select([k_ele, r_ele],
+               k_ele.c['entry_ent_seq'] == r_ele.c['entry_ent_seq'])
+    words = jd_engine.execute(s)
+    
+    for word in words:
+        word_l.append({'keb':word.keb, 'reb':word.reb})
+    
+    r_engine.execute(word_t.insert(), word_l)
+    print 'Filling database with word/reading data took '\
+            '%s seconds' % (time.time() - start)
+
+def get_meta():
         engine = create_engine('sqlite:///' + READINGS_PATH)
-        meta = MetaData()
-        meta.bind = engine
-        return meta
-
-def get_kanjidic_meta():
-        engine = create_engine('sqlite:///' + KANJIDIC_PATH)
-        meta = MetaData()
-        meta.bind = engine
-        return meta
-
-def get_jmdict_meta():
-        engine = create_engine('sqlite:///' + JMDICT_PATH)
         meta = MetaData()
         meta.bind = engine
         return meta
--- ryuujouji/reading.py
+++ ryuujouji/reading.py
@@ -11,30 +11,40 @@ from sqlalchemy.sql import select, and_, or_
 import tools
 import db
 
-
 class Segment():
-    unit = ''
-    reading = ''
-    info = ''
+    unit = None
+    reading = None
+    reading_id = None
+    info = None
+    index = 0
     
-    def __init__(self, unit, reading, info=''):
+    def __init__(self, unit, reading, reading_id, index, info=''):
         self.unit = unit
         self.reading = reading
+        self.reading_id = reading_id
+        self.index = index
         self.info = info
-        
-    
 
 indent = ''
-meta = db.get_readings_meta()
+meta = db.get_meta()
 meta.reflect()
 reading_t = meta.tables['reading']
+word_t = meta.tables['word']
+solution_t = meta.tables['solution']
+segment_t = meta.tables['segment']
+
 r_engine = meta.bind
-def get_individual_readings(word, reading, segments=[], found="", solutions=[]):
+def get_individual_readings(word, reading, segments=None, found=None, solutions=None):
+    
+    if solutions == None: solutions = []
+    if found == None: found = ""
+    if segments == None: segments = []
+    
     if len(word) == 0:
-        print "Nothing left to solve. Appending this one as a solution."
+#        print "Nothing left to solve. Appending this one as a solution."
         solutions.append(segments)
-    else:
-        print "\nSolving: %s  ==  %s          <--> Root: %s" % (word, reading, found)
+#    else:
+#        print "\nSolving: %s  ==  %s          <--> Root: %s" % (word, reading, found)
 
     #for char in word:
     if len(word) > 0:
@@ -43,30 +53,31 @@ def get_individual_readings(word, reading, segments=[], found="", solutions=[]):
             #if ord(char) in KATAKANA_RANGE:
             #    char = tools.kata_to_hira(char)
             if len(reading) == 0:
-                print "No readings left to match. Part of word left: ", word
+                pass
+#                print "No readings left to match. Part of word left: ", word
             else:
                 if char == reading[0]:
-                    print "--> Removed:  " + char
+#                    print "--> Removed:  " + char
                     tmp_found = found + char
                     tmp_word = word[1:]
                     tmp_reading = reading[1:]
                     tmp_segments = copy.copy(segments)
-                    tmp_segments.append(Segment(char, char, 'kana'))
+                    tmp_segments.append(Segment(char, char, 0, 0, 'kana'))
                     get_individual_readings(tmp_word, tmp_reading, tmp_segments,
                                             tmp_found, solutions)
-                else:
-                    print "%s is definitely not %s. Abort attempt." % (char, reading[0])
+#                else:
+ #                   print "%s is definitely not %s. Abort attempt." % (char, reading[0])
         else:
             s = select([reading_t], reading_t.c['character'] == char)
             char_readings = r_engine.execute(s).fetchall()
 
-            print "0th character is a kanji: = " + char
+#            print "0th character is a kanji: = " + char
 
             if char_readings == None:    #TODO: Handle non-kanji and non-kana characters
-                print "Need to abort gracefully!"
+#                print "Need to abort gracefully!"
                 return None
-            for r in char_readings:
-                print indent + "\t" + r.reading + "\t\t" + r.affix
+#            for r in char_readings:
+#                print indent + "\t" + r.reading + "\t\t" + r.affix
 
             for cr in char_readings:
                 #print "     " + r.reading + \t + r.affix + \t + str(r.has_okurigana)
@@ -80,13 +91,13 @@ def get_individual_readings(word, reading, segments=[], found="", solutions=[]):
                     ot = word[1:ol + 1] #the portion we want to test as okurigana
 
                     if r == reading[:rl] and ot == o:
-                        print "Match found for reading %s with okurigana %s" % (r, o)
+#                        print "Match found for reading %s with okurigana %s" % (r, o)
                         tmp_found = found + r + o
                         tmp_word = word[ol + 1:]
                         tmp_reading = reading[ol + rl:]
                         tmp_segments = copy.copy(segments)
-                        tmp_segments.append(Segment(char, r, 'kanji with okurigana (%s)'%o))
-                        tmp_segments.append(Segment(o, o, 'okurigana'))
+                        tmp_segments.append(Segment(char, r, 0, 0, 'kanji with okurigana (%s)'%o))
+                        tmp_segments.append(Segment(o, o, 0, 0, 'okurigana'))
                         get_individual_readings(tmp_word, tmp_reading, tmp_segments, tmp_found, solutions)
                 else:
                     r = cr.reading
@@ -96,19 +107,44 @@ def get_individual_readings(word, reading, segments=[], found="", solutions=[]):
                         r = tools.kata_to_hira(r)
 
                     if reading.startswith(r):
-                        print "Match found for reading %s: starting with %s" % (reading, r)
+#                        print "Match found for reading %s: starting with %s" % (reading, r)
                         tmp_found = found + r
                         tmp_word = word[1:]
                         tmp_reading = reading[rl:]
                         tmp_segments = copy.copy(segments)
-                        tmp_segments.append(Segment(char, r, 'kanji'))
+                        tmp_segments.append(Segment(char, r, 0, 0, 'kanji'))
                         get_individual_readings(tmp_word, tmp_reading, tmp_segments, tmp_found, solutions)
-            print "No further parsing possible for reading starting with %s and ending with %s.\n     Up one level." % (found, word)
+#            print "No further parsing possible for reading starting with %s and ending with %s.\n     Up one level." % (found, word)
     return solutions
 
+solution_l = []
+segment_l = []
 
+def fill_solutions():
+    sol_id = 0
+    
+    s = select([word_t])
+    words = r_engine.execute(s)
+    for word in words:
+        print "STARTING THIS WORD ==============>", word.keb, word.reb
+        solutions = get_individual_readings(word.keb, word.reb)
+        for s in solutions:
+            sol_id +=1
+            solution_l.append({'id':sol_id, 'word_id':word.id})
+            for seg in s:
+                #print 'seg == ' , seg.unit
+                segment_l.append({'solution_id':sol_id,
+                                  'reading_id':seg.reading_id,
+                                  'index':seg.index})
+    r_engine.execute(solution_t.insert(), solution_l)
+    r_engine.execute(segment_t.insert(), segment_l)
+    
+                
+    
+    
 if __name__ == "__main__":
 
+    fill_solutions()
     #db_populate_words_by_reading()
 
 #    solutions = get_individual_readings(u"小牛", u"こうし")
@@ -116,14 +152,14 @@ if __name__ == "__main__":
 #    solutions = get_individual_readings(u"バス停", u"バスてい")
 #    solutions = get_individual_readings(u"一つ", u"ひとつ")
 #    solutions = get_individual_readings(u"非常事態", u"ひじょうじたい")
-    solutions = get_individual_readings(u"建て替える", u"たてかえる")
+#    solutions = get_individual_readings(u"建て替える", u"たてかえる")
 #    solutions = get_individual_readings(u"今日", u"きょう")
 #    solutions = get_individual_readings(u"小さい", u"ちいさい")
 #    solutions = get_individual_readings(u"鉄道公安官", u"てつどうこうあんかん")
 #    solutions = get_individual_readings(u"日帰り", u"ひがえり")
-    print "\n\n"
-    for i,sol in enumerate(solutions):
-        print "Solution #", i
-        for seg in sol:
-             print "%s -- %s  -- %s" % (seg.unit, seg.reading, seg.info)
+#    print "\n\n"
+#    for i,sol in enumerate(solutions):
+#        print "Solution #", i
+#        for seg in sol:
+#             print "%s -- %s  -- %s" % (seg.unit, seg.reading, seg.info)
 #    db_populate_words_by_reading()
