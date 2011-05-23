5784b204b659807be61365affe9e3699a9cd9bcb
HSM <ntsp.gm@gmail.com>
2011-05-23 11:18:02 +1000

    Added tool to dump kanji and readings into a text file.

---
+++ tools/dump_readings.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+import os
+from sqlalchemy import create_engine, MetaData
+from sqlalchemy.sql import select, or_
+ 
+KANJIDIC_PATH = "../dbs/kanjidic.sqlite"
+
+def dump_readings():
+    print "Dumping kanji/reading data..."
+    kd_engine = create_engine('sqlite:///' + KANJIDIC_PATH)
+    kd_meta = MetaData()
+    kd_meta.bind = kd_engine
+    kd_meta.reflect()
+
+    kd_reading = kd_meta.tables['reading']
+
+    s = select([kd_reading], or_(kd_reading.c['r_type'] == 'ja_on',
+                                 kd_reading.c['r_type'] == 'ja_kun'))
+    readings = kd_engine.execute(s)
+    f = open('kanji_readings', 'w')
+    for r in readings:
+        f.write(r.character_literal + ',' +r.reading + '\n')
+
+    print "Done."
+            
+            
+            
+if __name__ == "__main__":
+    if not os.path.exists(KANJIDIC_PATH):
+        print "No kanjidic database found at %s" % KANJIDIC_PATH
+        print "Cannot continue without it."
+    else:
+        dump_readings()
