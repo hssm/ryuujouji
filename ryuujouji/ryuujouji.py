6ae2f867452a40c537b933d5a778e22ef186f745
HSM <ntsp.gm@gmail.com>
2011-06-25 02:04:23 +1000

    Command line usage.

---
+++ ryuujouji/ryuujouji.py
@@ -0,0 +1,27 @@
+﻿# -*- coding: utf-8 -*-
+#Copyright (C) 2011 Houssam Salem <ntsp.gm@gmail.com>
+#License: GPLv3; http://www.gnu.org/licenses/gpl.txt
+
+import argparse
+
+parser = argparse.ArgumentParser(description='Find reading segments.')
+parser.add_argument('-f', type = argparse.FileType('r'), default = '-')
+parser.add_argument('--encoding', default='utf-8')
+parser.add_argument('-d', '--delimiter', default=u' ')
+args = parser.parse_args()
+
+import solver
+
+try:
+    for i, line in enumerate(args.f):
+        line = unicode(line, encoding = args.encoding)
+        line = line.rstrip()
+        try:
+            word, reading = line.split(args.delimiter, 1)
+            solver.print_segments(word, reading)
+            print
+        except ValueError, e:
+            print "Line %d: Cannot parse line -- [%s]" % (i, line)
+        
+except KeyboardInterrupt:
+    pass
